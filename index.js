b4e65bc496a95e7b3ba26c29e1e84488fe6977c7
anshi83 <singhanshika5448@gmail.com>
2025-08-28 18:19:09 +0530

    frontened connect

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const UserRouter = require('./routers/UserRouters');
 const ProductRouter = require('./routers/ProductRouter');
+const cors = require('cors');
 const app = express();
 const port = 5500;
 
 //middleware
+
+app.use(cors({
+    origin: ['http://localhost:3000']
+}));
 app.use(express.json());
 app.use('/user', UserRouter);
 app.use('/product', ProductRouter);
